2138e1bc9d2655838f7c42b416797b42e716bdfa
InfernalWolf08 <jayinjoshi@gmail.com>
2026-02-02 20:34:35 -0800

    Initial commit

---
+++ src/main/java/com/github/InfernalWolf/Scpcraft.java
@@ -0,0 +1,13 @@
+package com.github.InfernalWolf;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Scpcraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+
+    public static void main(String[] args)
+    {}
+}
---
+++ src/main/java/com/github/InfernalWolf/client/ScpcraftClient.java
@@ -0,0 +1,10 @@
+package com.github.InfernalWolf.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScpcraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
